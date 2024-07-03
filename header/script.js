137bc3bcd787922eb5946f8b163cdfc50c492af4
Kawya Awarjana <kawya.evotech@gmail.com>
2024-07-03 13:52:00 +0530

    Kawya

---
+++ header/script.js
@@ -0,0 +1,8 @@
+function toggle() {
+
+    document.getElementById("dropdown").classList.toggle("show");
+    document.getElementById("drpItem").classList.toggle("hide");
+    document.getElementById("drpItem1").classList.toggle("hide");
+    document.getElementById("drpItem2").classList.toggle("hide");
+
+}
