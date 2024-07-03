0a445dea6ad9165d3de0aba99bd8f5d212d2fb19
Kawya Awarjana <kawya.evotech@gmail.com>
2024-07-04 01:13:36 +0530

    Kawya

---
+++ package/tour.js
@@ -0,0 +1,29 @@
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 5000); // Change image every 2 seconds
+}
+
+let slideIndex1 = 0;
+showSlides1();
+
+function showSlides1() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides1");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex1++;
+  if (slideIndex1 > slides.length) {slideIndex1 = 1}
+  slides[slideIndex1-1].style.display = "block";
+  setTimeout(showSlides1, 5000); // Change image every 2 seconds
+}
