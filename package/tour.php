f92acd99ea161e852893b0149e86e53db6403b23
Kawya Awarjana <kawya.evotech@gmail.com>
2024-07-04 23:57:43 +0530

    tour

--- package/tour.php
+++ package/tour.php
@@ -20,11 +20,15 @@
 
 
     <section class="to--sec1" id="DAY">
+        <div class="tp--sec1--div2 heading--pack01">
+            <span class="tp--sec1--span1">DAY TOURS</span>
+        </div>
+
+        <section class="to--sec1">
+        </section>
+
         <div class="to--sec1--div1">
-            <div class="tp--sec1--div2">
-                <span class="tp--sec1--span1">DAY TOURS</span>
-                <hr class="tp-sec1-hr1">
-            </div>
+
             <div class="tp--sec1--div3">
 
                 <div class="tp--sec1--div4">
@@ -41,25 +45,32 @@
                             </div>
 
                             <div class="mySlides fade">
-                                <img src="../resoursers/img/2.jpg" style="width:100%">
+                                <img src="../resoursers/img/2.png" style="width:100%">
                             </div>
 
                             <div class="mySlides fade">
                                 <img src="../resoursers/img/3.jpg" style="width:100%">
                             </div>
                             <div class="mySlides fade">
-                                <img src="../resoursers/img/4.jpg" style="width:100%">
+                                <img src="../resoursers/img/4.png" style="width:100%">
                             </div>
                         </div>
                         <br>
                         <!-- Slideshow container -->
                         <div class="tp--sec1--div6">
                             <span class="tp--sec1--span3">
-                                Main Destinations:-Weligama,Dikwella,Rawana waterfall,Yala Udawalawa Safari<br>
-                                Included:-<br>
-                                Morning Safari:-Pick Up and Drop Car/Breakfast/safari Vehical/Water Bottle <br>
-                                Evening Safari:-Pick Up and Drop Car/Refreshment Drink/safari Vehical/Water Bottle
-
+                                <b>Main Destinations:-</b><br> Weligama,Dikwella,Rawana waterfall,Yala Udawalawa Safari<br><br>
+                                <b> Included:-</b><br>
+                                <table>
+                                    <tr>
+                                        <td><b>Morning&nbsp;Safari:-</b></td>
+                                        <td>Pick Up and Drop Car/Breakfast/safari Vehical/Water Bottle</td>
+                                    </tr>
+                                    <tr>
+                                        <td><b>Evening&nbsp;Safari:-</b></td>
+                                        <td>Pick Up and Drop Car/Refreshment Drink/safari Vehical/Water Bottle</td>
+                                    </tr>
+                                </table>
                             </span>
 
                             <button class="tp--sec1--btn1">WHATSAPP</button>
@@ -88,31 +99,31 @@
 
                             <!-- Full-width images with number and caption text -->
                             <div class="mySlides1 fade">
-                                <img src="../resoursers/" style="width:100%">
+                                <img src="../resoursers/img/1.jpg" style="width:100%">
                             </div>
 
                             <div class="mySlides1 fade">
-                                <img src="../resoursers/" style="width:100%">
+                                <img src="../resoursers/img/2.png" style="width:100%">
                             </div>
 
                             <div class="mySlides1 fade">
-                                <img src="../resoursers/" style="width:100%">
+                                <img src="../resoursers/img/3.jpg" style="width:100%">
                             </div>
                             <div class="mySlides1 fade">
-                                <img src="../resoursers/" style="width:100%">
+                                <img src="../resoursers/img/4.png" style="width:100%">
                             </div>
                         </div>
                         <br>
                         <!-- Slideshow container -->
                         <div class="tp--sec1--div6">
                             <span class="tp--sec1--span3">
-                                Main Destinations:-Ambuluwawa,Damro tea Plantation,Gregary Lake<br>
-                                Included:-<br>
+                                <b>Main Destinations:- </b> <br>Ambuluwawa,Damro tea Plantation,Gregary Lake<br><br>
+                                <b>Included:-</b><br>
                                 Pick up and Drop Vehical,Lunch(Helabojun),Waterbottle.
 
                             </span>
 
-                            <button class="tp--sec1--btn1">WHATSAPP</button>
+                            <button class="tp--sec1--btn1"> WHATSAPP</a></button>
                         </div>
                     </div>
 
@@ -137,32 +148,32 @@
 
                             <!-- Full-width images with number and caption text -->
                             <div class="mySlides2 fade">
-                                <img src="../resoursers/" style="width:100%">
+                                <img src="../resoursers/img/1.jpg" style="width:100%">
                             </div>
 
                             <div class="mySlides2 fade">
-                                <img src="../resoursers/" style="width:100%">
+                                <img src="../resoursers/img/2.png" style="width:100%">
                             </div>
 
                             <div class="mySlides2 fade">
-                                <img src="../resoursers/" style="width:100%">
+                                <img src="../resoursers/img/3.jpg" style="width:100%">
                             </div>
                             <div class="mySlides2 fade">
-                                <img src="../resoursers/" style="width:100%">
+                                <img src="../resoursers/img/4.png" style="width:100%">
                             </div>
                         </div>
                         <br>
                         <!-- Slideshow container -->
                         <div class="tp--sec1--div6">
                             <span class="tp--sec1--span3">
-                                Main Destinations:- <br>
-                                Morning Safari Destinations(Start at 5 a.m):-Seegiriya,Pidurangala,Culture
-                                Village,Safari <br>
-                                Morning Safari incudes:-Pick Up and Drop vehical/Safari jeep/refreshment/water
-                                bottle/lunch<br>
-                                Morning Safari Destinations(Start at 9a.m):-culture village,tuk tuk safari,boat
-                                safari,jeep safari <br>
-                                Morning Safari incudes:- Pick Up and Drop Car/Refreshment Drink/safari Vehical/Water
+                            <b>Main Destinations:-</b> <br><br>
+                            <b>Morning Safari Destinations<br>(Start at 5 a.m):-</b><br>Seegiriya,Pidurangala,Culture
+                                Village,Safari <br><br>
+                                <b>Morning Safari incudes:-</b><br>Pick Up and Drop vehical/Safari jeep/refreshment/water
+                                bottle/lunch<br><br>
+                                <b>Morning Safari Destinations<br>(Start at 9a.m):-</b><br>culture village,tuk tuk safari,boat
+                                safari,jeep safari <br><br>
+                                <b>Morning Safari incudes:- </b><br>Pick Up and Drop Car/Refreshment Drink/safari Vehical/Water
                                 Bottle/lunch
 
                             </span>
@@ -192,26 +203,26 @@
 
                             <!-- Full-width images with number and caption text -->
                             <div class="mySlides3 fade">
-                                <img src="../resoursers/" style="width:100%">
+                                <img src="../resoursers/img/1.jpg" style="width:100%">
                             </div>
 
                             <div class="mySlides3 fade">
-                                <img src="../resoursers/" style="width:100%">
+                                <img src="../resoursers/img/2.png" style="width:100%">
                             </div>
 
                             <div class="mySlides3 fade">
-                                <img src="../resoursers/" style="width:100%">
+                                <img src="../resoursers/img/3.jpg" style="width:100%">
                             </div>
                             <div class="mySlides3 fade">
-                                <img src="../resoursers/" style="width:100%">
+                                <img src="../resoursers/img/4.png" style="width:100%">
                             </div>
                         </div>
                         <br>
                         <!-- Slideshow container -->
                         <div class="tp--sec1--div6">
                             <span class="tp--sec1--span3">
-                                Main Destinations(Start at 9.30a.m):-culture village,Safari <br>
-                                Includes:-Pickup and Drop vehical/water bottle/Refreshment drink/Safari jeep
+                                <b>Main Destinations<br>(Start at 9.30a.m):-</b><br> village,Safari <br><br>
+                                <b>Includes:-</b><br>Pickup and Drop vehical/water bottle/Refreshment drink/Safari jeep
 
 
                             </span>
@@ -231,11 +242,18 @@
     <!-- begin city tours -->
 
     <section class="to--sec1" id="CITY">
+        <div class="tp--sec1--div2">
+            <span class="tp--sec1--span1">CITY TOURS</span>
+            <!-- <hr class="tp-sec1-hr1"> -->
+        </div>
+
+
+        <section class="to--sec1">
+        </section>
+
+
         <div class="to--sec1--div1">
-            <div class="tp--sec1--div2">
-                <span class="tp--sec1--span1">CITY TOURS</span>
-                <hr class="tp-sec1-hr1">
-            </div>
+
             <div class="tp--sec1--div3">
 
                 <div class="tp--sec1--div4">
@@ -248,26 +266,26 @@
 
                             <!-- Full-width images with number and caption text -->
                             <div class="mySlides4 fade">
-                                <img src="../resoursers/" style="width:100%">
+                                <img src="../resoursers/img/1.jpg" style="width:100%">
                             </div>
 
                             <div class="mySlides4 fade">
-                                <img src="../resoursers/" style="width:100%">
+                                <img src="../resoursers/img/2.png" style="width:100%">
                             </div>
 
                             <div class="mySlides4 fade">
-                                <img src="../resoursers/" style="width:100%">
+                                <img src="../resoursers/img/3.jpg" style="width:100%">
                             </div>
                             <div class="mySlides4 fade">
-                                <img src="../resoursers/" style="width:100%">
+                                <img src="../resoursers/img/4.png" style="width:100%">
                             </div>
                         </div>
                         <br>
                         <!-- Slideshow container -->
                         <div class="tp--sec1--div6">
                             <span class="tp--sec1--span3">
-                                Main Destinations:-Coconut treehil ,Parrot Rock ,Secreat beach<br>
-                                Includes:-Pickup and drop vehicale,water bottle,refreshment drink
+                                <b>Main Destinations:-</b><br> treehil ,Parrot Rock ,Secreat beach<br><br>
+                                <b>Includes:-</b><br> Pickup and drop vehicale,water bottle,refreshment drink
 
 
                             </span>
@@ -297,27 +315,27 @@
 
                             <!-- Full-width images with number and caption text -->
                             <div class="mySlides5 fade">
-                                <img src="../resoursers/" style="width:100%">
+                                <img src="../resoursers/img/1.jpg" style="width:100%">
                             </div>
 
                             <div class="mySlides5 fade">
-                                <img src="../resoursers/" style="width:100%">
+                                <img src="../resoursers/img/2.png" style="width:100%">
                             </div>
 
                             <div class="mySlides5 fade">
-                                <img src="../resoursers/" style="width:100%">
+                                <img src="../resoursers/img/3.jpg" style="width:100%">
                             </div>
                             <div class="mySlides5 fade">
-                                <img src="../resoursers/" style="width:100%">
+                                <img src="../resoursers/img/4.png" style="width:100%">
                             </div>
                         </div>
                         <br>
                         <!-- Slideshow container -->
                         <div class="tp--sec1--div6">
                             <span class="tp--sec1--span3">
-                                Main Destinations:- Galle Fort ,Jubgle beach,Kalawalla beach,Japanese temple,Koggala
-                                stilt fishing<br>
-                                Includes:-Pickup and drop vehicale,water bottle,refreshment drink
+                                <b> Main Destinations:-</b><br> Galle Fort ,Jubgle beach,Kalawalla beach,Japanese temple,Koggala
+                                stilt fishing<br><br>
+                                <b>Includes:-</b> <br>Pickup and drop vehicale,water bottle,refreshment drink
 
                             </span>
 
@@ -346,27 +364,27 @@
 
                             <!-- Full-width images with number and caption text -->
                             <div class="mySlides6 fade">
-                                <img src="../resoursers/" style="width:100%">
+                                <img src="../resoursers/img/1.jpg" style="width:100%">
                             </div>
 
                             <div class="mySlides6 fade">
-                                <img src="../resoursers/" style="width:100%">
+                                <img src="../resoursers/img/2.png" style="width:100%">
                             </div>
 
                             <div class="mySlides6 fade">
-                                <img src="../resoursers/" style="width:100%">
+                                <img src="../resoursers/img/3.jpg" style="width:100%">
                             </div>
                             <div class="mySlides6 fade">
-                                <img src="../resoursers/" style="width:100%">
+                                <img src="../resoursers/img/4.png" style="width:100%">
                             </div>
                         </div>
                         <br>
                         <!-- Slideshow container -->
                         <div class="tp--sec1--div6">
                             <span class="tp--sec1--span3">
-                                Main Destinations:-Ambuluwawa,Kandy temple,Peradeniya botanical garden,Kandy
-                                viewpoint,Nelligala Temple<br>
-                                Includes:-Pickup and drop vehical,water bottle,refreshment drink
+                                <b>Main Destinations:-</b><br>Ambuluwawa,Kandy temple,Peradeniya botanical garden,Kandy
+                                viewpoint,Nelligala Temple<br><br>
+                                <b>Includes:-</b><br>Pickup and drop vehical,water bottle,refreshment drink
 
 
                             </span>
