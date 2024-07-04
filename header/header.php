938b7b5d2c4093df38e1870dceecb45974b18e98
Kawya Awarjana <kawya.evotech@gmail.com>
2024-07-05 00:21:55 +0530

    header

--- header/header.php
+++ header/header.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width,initial-scale=1.0" />
@@ -8,7 +7,6 @@
     <link rel="icon" href="../resoursers/logo.png" />
     <link rel="stylesheet" href="header.css" />
 </head>
-
 <body class="header--body">
     <!-- mobile -->
     <section class="header--sec1">
@@ -16,36 +14,41 @@
             <div class="header--sec1--div2">
                 <img src="../resoursers/img/toggle.png" onclick="toggle();" class="header--toggle" />
             </div>
-
             <div class="header--sec1--div3">
                 <img src="../resoursers/img/logo.png" class="header--logo" />
             </div>
         </div>
-
         <div class="dropdown" id="dropdown">
             <span class="dropdown--item hide" id="drpItem"><a href="../Home/index.php" class="header--a">HOME</a></span>
             <span class="dropdown--item hide" id="drpItem1">
+                <select class="dropdown--lg">
+                    <option class="dropdown--item--sm">
+                        <a href="#" class="header--a">
                 <select class="dropdown--lg" id="haederSelect" onchange="toggleHeaderSelect();">
                     <option class="dropdown--item--sm" value="../package/tour.php">
                             TOUR PACKAGES
+                        </a>
                     </option>
+                    <option class="dropdown--item--sm">
+                        <a href="../package/tour.php#CITY" class="header--a">
                     <option class="dropdown--item--sm" value="../package/tour.php#CITY">
                             CITY TOURS
+                        </a>
                     </option>
+                    <option class="dropdown--item--sm">
+                        <a href="../package/tour.php#DAY" class="header--a">
                     <option class="dropdown--item--sm" value="../package/tour.php#DAY">
                             DAY TOURS
+                        </a>
                     </option>
                 </select>
             </span>
             <span class="dropdown--item hide" id="drpItem2"><a href="../contact/contact.php" class="header--a">CONTACT US</a></span>
         </div>
-
         <div class="call--now--btn">
             <span class="call--now--txt"><a href="tel:+94 77 361 4654" class="call--now--txt">CALL NOW!</a></span>
         </div>
-
     </section>
-
     <!-- large screen -->
     <section class="header--sec2">
         <div class="header--sec2--div1">
@@ -57,6 +60,10 @@
                     <a href="../Home/index.php" class="header--a">HOME</a>
                 </div>
                 <div class="header--sec2--div5 dropdown--item--large  dropdown--lg">
+                    <select class="header--sec2--div5 dropdown--lg">
+                        <option class="header--sec2--div5 dropdown--item--lg"><a href="#" class="header--a">TOUR PACKAGES</a></option>
+                        <option class="header--sec2--div5 dropdown--item--lg" onclick="window.location='../package/tour.php#CITY';"><a href="../package/tour.php#CITY" class="header--a">CITY TOURS</a></option>
+                        <option class="header--sec2--div5 dropdown--item--lg" onclick="window.location='../package/tour.php#CITY';"><a href="../package/tour.php#DAY" class="header--a">DAY TOURS</a></option>
                     <select class="header--sec2--div5 dropdown--lg" onchange="toggleHeaderSelectLarge();" id="headerSelectLarge">
                         <option class="header--sec2--div5 dropdown--item--lg" value="../package/tour.php">TOUR PACKAGES</option>
                         <option class="header--sec2--div5 dropdown--item--lg" value="../package/tour.php#CITY">CITY TOURS</option>
@@ -68,17 +75,12 @@
                 </div>
             </div>
         </div>
-
         <div class="call--now--btn">
             <span class="call--now--txt"><a href="tel:+94 77 361 4654" class="call--now--txt">CALL NOW!</a></span>
         </div>
-
     </section>
-
     <div class="header--spaceDiv">
     </div>
-
     <script src="script.js"></script>
 </body>
-
 </html>
