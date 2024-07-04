fde74e6bbe372ec013141f278d4e203a13538717
Kawya Awarjana <kawya.evotech@gmail.com>
2024-07-05 00:50:46 +0530

    final

--- contact/contact.php
+++ contact/contact.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Contact Us</title>
+    <title>KHK TOURS | CONTACT US</title>
     <link rel="icon" href="../resoursers/img/logo.png" />
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="../header/header.css" />
--- package/tour.php
+++ package/tour.php
@@ -4,7 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>TOUR PACKAGES</title>
+    <title>KHK TOURS | PACKAGES</title>
+    <link rel="icon" href="../resoursers/img/logo.png" />
     <link rel="stylesheet" href="tour.css">
     <link rel="stylesheet" href="../header/header.css" />
     <link rel="stylesheet" href="../footer/footer.css" />
