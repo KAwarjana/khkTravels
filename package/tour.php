aca1294f7d14a80cffc81a271b885ce98146cc20
Kawya Awarjana <kawya.evotech@gmail.com>
2024-07-09 02:41:49 +0530

    packages

--- package/tour.php
+++ package/tour.php
@@ -60,16 +60,52 @@
                         <!-- Slideshow container -->
                         <div class="tp--sec1--div6">
                             <span class="tp--sec1--span3">
-                                <b>Main Destinations:-</b><br> Weligama,Dikwella,Rawana waterfall,Yala Udawalawa Safari<br><br>
-                                <b> Included:-</b><br>
+                                Enjoy a transfer from the cities of Hirikatiya, Mirissa, Weligama or Galle to your hotel in the Ella area. Stop at Yala National Park and Udawalawe National Park to witness the wild animals in their natural habitats along the way and enjoy the beauty of the environment with an array of interesting and never-before-seen experiences at your destination.
+                                <br><br>
+                                <!-- <b>Main Destinations:-</b><br> Weligama,Dikwella,Rawana waterfall,Yala Udawalawa Safari<br><br> -->
+
+                                <b> INCLUDING</b>
+                                <ul>
+                                    <li>Pick up and drop vehicle</li>
+                                    <li>Safari Jeep</li>
+                                    <li>Well experience driver and guide</li>
+                                </ul><br>
                                 <table>
                                     <tr>
-                                        <td><b>Morning&nbsp;Safari:-</b></td>
-                                        <td>Pick Up and Drop Car/Breakfast/safari Vehical/Water Bottle</td>
+                                        <td><b>MORNING&nbsp;SAFARI:-</b></td>
+                                        <td>
+                                            <ul type="Square">
+                                                <li>Breakfast</li>
+                                            </ul>
+                                        </td>
                                     </tr>
                                     <tr>
-                                        <td><b>Evening&nbsp;Safari:-</b></td>
-                                        <td>Pick Up and Drop Car/Refreshment Drink/safari Vehical/Water Bottle</td>
+                                        <td></td>
+                                        <td>
+                                            <ul type="Square" class="water">
+                                                <li>Water&nbsp;Bottle</li>
+                                            </ul>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                    </tr>
+                                    <tr>
+                                    </tr>
+                                    <tr>
+                                        <td><b>EVENING&nbsp;SAFARI:-</b></td>
+                                        <td>
+                                            <ul type="Square">
+                                                <li>Refreshment Drink</li>
+                                            </ul>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td></td>
+                                        <td>
+                                            <ul type="Square" class="water">
+                                                <li>Water&nbsp;Bottle</li>
+                                            </ul>
+                                        </td>
                                     </tr>
                                 </table>
                             </span>
@@ -118,9 +154,23 @@
                         <!-- Slideshow container -->
                         <div class="tp--sec1--div6">
                             <span class="tp--sec1--span3">
-                                <b>Main Destinations:- </b> <br>Ambuluwawa,Damro tea Plantation,Gregary Lake<br><br>
-                                <b>Included:-</b><br>
-                                Pick up and Drop Vehical,Lunch(Helabojun),Waterbottle.
+                                When you travel from Kandy to Ella city we hope to make your trip an unforgettable trip of your life. Therefore, you can also visit Ambuluawa Tower and Damro Tea Factory. Also, have lunch in Nuwara Eliya and see the Gregory Lake and Sita Amman Temple and the beauty of Nuwara Eliya. You can visit Ella city from Kandy starting at 7.00 am from the city and it will take you 10 hours to travel.
+                                <br><br>
+                                <b>MAIN DESTINATIONS</b>
+                                <ul>
+                                    <li>Ambuluwawa </li>
+                                    <li>Damro tea Plantation</li>
+                                    <li>Gregory Lake</li>
+                                </ul><br>
+                                <b> INCLUDING</b>
+                                <ul>
+                                    <li>Pick and drop Vehicle your destination </li>
+                                    <li>Water bottle and refreshment</li>
+                                    <li>Lunch (Helabojun)</li>
+                                    <li>Well experience Guide</li>
+                                    <li>Hotel pickup and drop off</li>
+                                    <li>Pick and drop Vehicle your destination</li>
+                                </ul><br>
 
                             </span>
 
@@ -167,15 +217,34 @@
                         <!-- Slideshow container -->
                         <div class="tp--sec1--div6">
                             <span class="tp--sec1--span3">
-                            <b>Main Destinations:-</b> <br><br>
-                            <b>Morning Safari Destinations<br>(Start at 5 a.m):-</b><br>Seegiriya,Pidurangala,Culture
+                                You can visit Minneriya or Kaudulla National Parks on your way from Colombo to Trincomalee or Nilaveli and experience the way wild animals behave in the environment. You can also visit the cultural village. It is an experience you will never have in your life. You can also have your lunch there. You will also enjoy a boat ride and a cart ride on the lake in the village. It starts at 8.00 am and lasts for 12 hours.
+                                <!-- <b>Main Destinations:-</b> <br><br>
+                                <b>Morning Safari Destinations<br>(Start at 5 a.m):-</b><br>Seegiriya,Pidurangala,Culture
                                 Village,Safari <br><br>
                                 <b>Morning Safari incudes:-</b><br>Pick Up and Drop vehical/Safari jeep/refreshment/water
                                 bottle/lunch<br><br>
                                 <b>Morning Safari Destinations<br>(Start at 9a.m):-</b><br>culture village,tuk tuk safari,boat
                                 safari,jeep safari <br><br>
                                 <b>Morning Safari incudes:- </b><br>Pick Up and Drop Car/Refreshment Drink/safari Vehical/Water
-                                Bottle/lunch
+                                Bottle/lunch -->
+                                <br><br><br>
+                                <b> INCLUDING</b>
+                                <ul>
+                                    <li>Safari Jeep</li>
+                                    <li>Well experience Guide</li>
+                                    <li>With /Without national park entrance frees with taxes</li>
+                                    <li>Hotel pickup and drop off</li>
+                                    <li>Pick and drop Vehicle your destination</li>
+                                    <li>4×4 Luxury safari Jeep</li>
+                                    <li>Safari Jeep Driver with extensive experience</li>
+                                    <li>Lunch for culture village (Cart tour/ Boat tour)</li>
+                                </ul><br>
+
+                                <b>AMENITIES </b>
+                                <ul>
+                                    <li>Fresh fruit platter </li>
+                                    <li>Water bottles during tour</li>
+                                </ul>
 
                             </span>
 
@@ -222,9 +291,92 @@
                         <!-- Slideshow container -->
                         <div class="tp--sec1--div6">
                             <span class="tp--sec1--span3">
-                                <b>Main Destinations<br>(Start at 9.30a.m):-</b><br> village,Safari <br><br>
-                                <b>Includes:-</b><br>Pickup and Drop vehical/water bottle/Refreshment drink/Safari jeep
+                            You can visit Minneriya or Kaudulla National Parks on your way from Kandy to Trincomalee or Nilaveli and experience the way wild animals behave in the environment. You can also visit the cultural village. It is an experience you will never have in your life. You can also have your lunch there. You will also enjoy a boat ride and a cart ride on the lake in the village. It starts at 9.30 am and lasts for 10 hours.
+                                
+                            <br><br><br>
+                                <b> INCLUDING</b>
+                                <ul>
+                                    <li>Safari Jeep</li>
+                                    <li>Well experience Guide</li>
+                                    <li>With /Without national park entrance frees with taxes</li>
+                                    <li>Hotel pickup and drop off</li>
+                                    <li>Pick and drop Vehicle your destination</li>
+                                    <li>4×4 Luxury safari Jeep</li>
+                                    <li>Safari Jeep Driver with extensive experience</li>
+                                    <li>Lunch for culture village (Cart tour/ Boat tour)</li>
+                                </ul><br>
 
+                                <b>AMENITIES </b>
+                                <ul>
+                                    <li>Fresh fruit platter </li>
+                                    <li>Water bottles during tour</li>
+                                </ul>
+
+                            </span>
+
+                            <button class="tp--sec1--btn1" onclick="document.location='https://wa.me/94773614654'">WHATSAPP</button>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+    <section class="to--sec1">
+        <div class="to--sec1--div1">
+
+            <div class="tp--sec1--div3">
+
+                <div class="tp--sec1--div4">
+                    <span class="tp--sec1--span2">
+                    Ella One day tour
+                    </span>
+                    <div class="tp--sec1--div5">
+                        <!-- Slideshow container -->
+                        <div class="slideshow-container">
+
+                            <!-- Full-width images with number and caption text -->
+                            <div class="mySlides3 fade">
+                                <img src="../resoursers/img/1.jpg" style="width:100%">
+                            </div>
+
+                            <div class="mySlides3 fade">
+                                <img src="../resoursers/img/2.png" style="width:100%">
+                            </div>
+
+                            <div class="mySlides3 fade">
+                                <img src="../resoursers/img/3.jpg" style="width:100%">
+                            </div>
+                            <div class="mySlides3 fade">
+                                <img src="../resoursers/img/4.png" style="width:100%">
+                            </div>
+                        </div>
+                        <br>
+                        <!-- Slideshow container -->
+                        <div class="tp--sec1--div6">
+                            <span class="tp--sec1--span3">
+                            You can visit Minneriya or Kaudulla National Parks on your way from Kandy to Trincomalee or Nilaveli and experience the way wild animals behave in the environment. You can also visit the cultural village. It is an experience you will never have in your life. You can also have your lunch there. You will also enjoy a boat ride and a cart ride on the lake in the village. It starts at 9.30 am and lasts for 10 hours.
+                                
+                            <br><br><br>
+                                <b> INCLUDING</b>
+                                <ul>
+                                    <li>Safari Jeep</li>
+                                    <li>Well experience Guide</li>
+                                    <li>With /Without national park entrance frees with taxes</li>
+                                    <li>Hotel pickup and drop off</li>
+                                    <li>Pick and drop Vehicle your destination</li>
+                                    <li>4×4 Luxury safari Jeep</li>
+                                    <li>Safari Jeep Driver with extensive experience</li>
+                                    <li>Lunch for culture village (Cart tour/ Boat tour)</li>
+                                </ul><br>
+
+                                <b>AMENITIES </b>
+                                <ul>
+                                    <li>Fresh fruit platter </li>
+                                    <li>Water bottles during tour</li>
+                                </ul>
 
                             </span>
 
